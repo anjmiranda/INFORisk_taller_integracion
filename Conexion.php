f8b920e61b33650bf274e8f452a3eb8ced8c22e0
freiregtr <ramon.freire.o@gmail.com>
2019-05-26 22:45:33 -0400

    modificacion archivo Conexion.php para trabajar en forma local

--- Conexion.php
+++ Conexion.php
@@ -5,7 +5,7 @@
         static public function conectar()
         {
             // metodo de conexion: direccion bbdd, nombre bbdd, user, pass
-            $link = new PDO("mysql:host=34.74.58.42;dbname=inforisk_bbdd", "root", "casa1matriz");
+            $link = new PDO("mysql:host=localhost;dbname=inforisk_bbdd", "root", "casa1matriz");
 
             $link -> exec("set names utf8");
             return $link;
